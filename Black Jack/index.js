aa2060449c01158dcb7fee2bc08f728ac9e00b9e
Mahak Juriani <juriani.mahak@gmail.com>
2024-05-27 22:07:26 +0530

    third commit - blackjack

---
+++ Black Jack/index.js
@@ -0,0 +1,37 @@
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    messageEl.innerText = message   
+}
+
+function newCard(){
+    console.log("Drawing a new card from the deck!")
+    let card = 7
+    sum+=card
+    renderGame()
+}
